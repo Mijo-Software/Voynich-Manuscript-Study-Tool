fef785b7d5b021d4e5792d4fb29f61bde740e620
Michael Johne <mjohne@users.noreply.github.com>
2019-12-26 17:25:00 +0100

    Add files via upload

---
+++ VoynichManuscriptStudyTool/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace VoynichManuscriptStudyTool
+{
+	/// <summary>
+	/// Program
+	/// </summary>
+	internal static class Program
+	{
+		/// <summary>
+		/// maion entrance point of the application
+		/// </summary>
+		[STAThread]
+		private static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(defaultValue: false);
+			using (MainForm mainForm = new MainForm())
+			{
+				Application.Run(mainForm: mainForm);
+			}
+		}
+	}
+}
---
+++ VoynichManuscriptStudyTool/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("Voynich Manuscript Study Tool")]
+[assembly: AssemblyDescription("The Voynich Manuscript Study Tool (VMST) is an application to browse an explore the Voynich Manuscript.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mijo Software")]
+[assembly: AssemblyProduct("Voynich Manuscript Study Tool")]
+[assembly: AssemblyCopyright("Copyright © 2019")]
+[assembly: AssemblyTrademark("Voynich Manuscript Study Tool")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("9ab2acd9-0266-4953-b4d6-f317d4281e8f")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// indem Sie "*" wie unten gezeigt eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.1.1")]
+[assembly: AssemblyFileVersion("0.0.1.1")]
+[assembly: NeutralResourcesLanguage("en")]
